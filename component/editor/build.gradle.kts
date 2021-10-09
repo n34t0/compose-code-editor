9fbc682b44cee45bf87c708a874dfb75017d1240
Alex <n34to0@gmail.com>
2021-10-09 12:15:07 +0300

    Move code from main to desktopMain

--- component/editor/build.gradle.kts
+++ component/editor/build.gradle.kts
@@ -3,8 +3,34 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm")
+    kotlin("multiplatform")
     id("org.jetbrains.compose") version "1.0.0-alpha4-build331"
+}
+
+kotlin {
+    jvm("desktop")
+
+    sourceSets {
+        named("commonMain") {
+            dependencies {
+                api(compose.runtime)
+                api(compose.foundation)
+                api(compose.material)
+                api(compose.materialIconsExtended)
+                api(project(":ipw:api"))
+            }
+        }
+        named("desktopMain") {
+            dependencies {
+                implementation(compose.uiTooling)
+                implementation(compose.desktop.currentOs)
+                implementation("io.github.microutils:kotlin-logging-jvm:2.0.10")
+                implementation("org.apache.logging.log4j:log4j-core:2.14.1")
+                implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
+                implementation(project(":ipw:lib"))
+            }
+        }
+    }
 }
 
 rootProject.apply {
@@ -21,22 +47,11 @@ repositories {
     google()
 }
 
-dependencies {
-    implementation(compose.uiTooling)
-    implementation(compose.desktop.currentOs)
-    implementation(compose.materialIconsExtended)
-    implementation("io.github.microutils:kotlin-logging-jvm:2.0.10")
-    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
-    implementation(project(":ipw:api"))
-    implementation(project(":ipw:lib"))
-}
-
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
 }
 
-compose.desktop {
+/*compose.desktop {
     application {
         mainClass = "com.github.n34t0.compose.codeEditor/MainKt"
         jvmArgs("-Xmx2048m")
@@ -48,4 +63,4 @@ compose.desktop {
             packageVersion = "1.0.0"
         }
     }
-}
+}*/

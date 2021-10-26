1218a8813f660e28a74b9e435a91f1c3dad6d712
Alex <n34to0@gmail.com>
2021-10-26 23:04:02 +0300

    Add publication workflows

--- component/platform/api/build.gradle.kts
+++ component/platform/api/build.gradle.kts
@@ -33,5 +33,9 @@ publishing {
 }
 
 signing {
+    val signingKeyId: String? by project
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
     sign(publishing.publications["mavenPlatformApi"])
 }
